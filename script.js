a26744b4012a471064519eebb57d8adf5d474c46
Eduardoo1233 <160926458+Eduardoo1233@users.noreply.github.com>
2026-03-01 02:55:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -66,3 +66,72 @@ function carregarTarefas() {
     };
     
     li.addEventListener("click", function () {
+     li.classList.toggle("concluida");
+      salvarTarefas();
+      contadorDeTarefas();
+    });
+    
+    const btnRemover = document.createElement("button");
+    btnRemover.textContent = "Remover";
+    
+    btnRemover.addEventListener("click", function () {   
+      li.remove();
+      salvarTarefas();
+      contadorDeTarefas();
+    });
+    
+    li.appendChild(btnRemover);
+    lista.appendChild(li);
+    contadorDeTarefas();
+  });
+  contadorDeTarefas();
+}
+
+//Contador de tarefas nao concluidas 
+
+const contadorText = document.createElement("p");
+contadorText.classList.add("contadorText");
+container.appendChild(contadorText);
+
+function contadorDeTarefas() {
+  const listaCompleta = document.querySelectorAll("#lista li");
+  const totalDeTarefas = listaCompleta.length;
+  
+  let contador = 0;
+  
+  listaCompleta.forEach(li => {
+    if(!li.classList.contains("concluida")) {
+      contador++;
+    };
+  });
+  
+  if(totalDeTarefas === 0) {
+    contadorText.textContent = "Você ainda não adicionou uma Tarefa!";
+  } else if (contador === 0) {
+      contadorText.textContent = "Parabens você concluiu todas as suas tarefas!"; 
+  } else if (contador === 1) {
+      contadorText.textContent = "Falta 1 tarefa a ser concluida";
+  } else {
+    contadorText.textContent = `Faltam ${contador} tarefas a serem concluidas!`;
+  };
+  const tarefasConcluidas = totalDeTarefas - contador;
+  btnRemoverConcluidas.disabled = tarefasConcluidas === 0;
+};
+
+//fim contador de tarefas nao concluidas
+
+const btnRemoverConcluidas = document.createElement("button");
+btnRemoverConcluidas.classList.add("btn-limpar");
+btnRemoverConcluidas.textContent = "Remover Tarefas Concluidas";
+container.appendChild(btnRemoverConcluidas);
+  
+btnRemoverConcluidas.addEventListener("click", function () {
+  document.querySelectorAll("#lista li").forEach(li => {
+    if (li.classList.contains("concluida")) {
+      li.remove();
+    };
+  });
+  salvarTarefas();
+  contadorDeTarefas();
+});
+carregarTarefas();
